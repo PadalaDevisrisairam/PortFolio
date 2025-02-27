64435c54f37ec05b1f394a308ddb12a4e2a7101c
PadalaDevisrisairam <devisri.padala@sasi.ac.in>
2025-02-27 15:16:51 +0530

    portfolio version 6

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar()
             <p>hello world</p>
             <p> this is deployed only</p>
             <a href="https://github.com/PadalaDevisrisairam/PortFolio">this is link to my github</a>
-           
+           <p>deployed with version 6</p>
         </div>
     )
 }
